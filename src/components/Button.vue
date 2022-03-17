8c56065d81c17567c303e5c8b71c5d50b244296c
thinkabyaroo <thinkabyaroo2911@gmail.com>
2022-03-17 13:38:09 +0630

    conpoment btn

---
+++ src/components/Button.vue
@@ -0,0 +1,20 @@
+<template>
+<button class="btn btn-primary">
+  {{ btnName }}
+</button>
+</template>
+
+<script>
+export default {
+  props: {
+    btnName: {
+      type: String,
+      default:"sample btn"
+    },
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,36 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="row my-3">
+      <div class="col-12">
+        <div class="card">
+          <div class="card-body">
+            <h1>Hello</h1>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+            <Button btn-name="save"></Button>
+          </div>
+        </div>
+        <div class="card">
+          <div class="card-body">
+            <h1>Hello</h1>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+            <Button></Button>
+          </div>
+        </div>
+
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
+import Button from "@/components/Button";
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+  components: {Button}
 }
 </script>
+
+<style scoped>
+
+</style>
